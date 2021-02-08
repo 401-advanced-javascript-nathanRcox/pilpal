cf2391c9b85925053cbd7446e4d50e84496ecbcd
Dina Ayoub <dinaayoub@hotmail.com>
2021-02-07 17:33:34 -0800

    added reducers

---
+++ src/store/user-reducer.js
@@ -0,0 +1,34 @@
+
+//category reducer
+let initialState = {
+}
+
+export const signup = (newUser) => {
+  return {
+    type: 'SIGNUP',
+    payload: newUser
+  }
+}
+
+export const signin = (user) => {
+  return {
+    type: 'SIGNIN',
+    payload: user
+  }
+}
+
+const userReducer = (state = initialState, action) => {
+  let { type, payload } = action;
+  switch (type) {
+    case 'SIGNUP':
+      //todo: do an API call here
+      return { payload };
+    case 'SIGNIN':
+      //todo: do an API call
+      return { payload };
+    default:
+      return state;
+  }
+}
+
+export default userReducer;
