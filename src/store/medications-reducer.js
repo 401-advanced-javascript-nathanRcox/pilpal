6bf895c3ad618ecf40f08719a527f11b3f9d6c6a
Dina Ayoub <dinaayoub@hotmail.com>
2021-02-07 17:33:52 -0800

    added reducers

---
+++ src/store/medications-reducer.js
@@ -0,0 +1,25 @@
+
+//category reducer
+let initialState = {
+  medications: []
+}
+
+export const addMedication = (newMedication) => {
+  return {
+    type: 'ADD',
+    payload: newMedication
+  }
+}
+
+const medicationReducer = (state = initialState, action) => {
+  let { type, payload } = action;
+  switch (type) {
+    case 'ADD':
+      //todo: do an API call here
+      return { ...state, activeCategory: payload };
+    default:
+      return state;
+  }
+}
+
+export default medicationReducer;
