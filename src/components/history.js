04e6d929a7104f649107c5ca5b0ef29862703adf
Nathan Rhead Cox <nrc4@hotmail.com>
2021-02-10 13:16:44 -0800

    Added links to history page.

--- src/components/history.js
+++ src/components/history.js
@@ -1,35 +1,49 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
-import { Text, Surface } from 'react-native-paper';
+import { Text, Surface, Button, Avatar, Card, Title, Paragraph } from 'react-native-paper';
+import { StyleSheet, ScrollView } from 'react-native';
 import { getAllMedHistory } from '../store/medication-history-reducer';
+// import { }
 
 const mapDispatchToProps = { getAllMedHistory };
 
 function History(props) {
 
+  const [history, setHistory] = useState('true');
+  const [oneMed, setOneMed] = useState('false');
+
+  const getOneDrugById = (drug) => {
+    console.log('DRUG:', drug);
+  }
+
   useEffect(() => {
-    console.log('PROPS on HISTORY:', props);
+    // console.log('PROPS on HISTORY:', props.history);
     getAllMedHistory(props.user)
   }, [props])
 
   return (
-    <>
-      <Text>Test</Text>
-      {console.log('PROPS in HISTORY Render', props)}
-      {props.history.medication_history.map(data => (
-        <Surface key={data._id}>
-          <Text>
-            Test {data.name}
-          </Text>
-          <Text>
-            {data.datetime}
-          </Text>
-          <Text>
-            {data.notes}
-          </Text>
-        </Surface>
+    <ScrollView>
+      <Button>Sort by Medication</Button>
+      {props.history.medication_history.map(drug => (
+
+        <Card style={styles.surface} key={drug._id}>
+          <Card.Title title={drug.name}  />
+          <Card.Content>
+            {/* <Title> {drug.name} </Title> */}
+            <Paragraph>
+              Date & Time: {drug.datetime}
+            </Paragraph>
+            <Paragraph>
+              Notes: {drug.notes}
+            </Paragraph>
+          </Card.Content>
+          <Card.Actions>
+            <Button>Edit</Button>
+            <Button onPress={() => getOneDrugById(drug._id)}>History</Button>
+          </Card.Actions>
+        </Card>
       ))}
-    </>
+    </ScrollView>
   );
 }
 const mapStateToProps = (state) => ({
@@ -39,3 +53,14 @@ const mapStateToProps = (state) => ({
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(History);
+
+const styles = StyleSheet.create({
+  surface: {
+    margin: 8,
+    // height: 100,
+    // width: 300,
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    // elevation: 4,
+  },
+});
--- src/store/medication-history-reducer.js
+++ src/store/medication-history-reducer.js
@@ -6,28 +6,28 @@ let initialState = {
 }
 
 export const getAllMedHistory = (payload) => dispatch => {
-  // console.log('payload on history', payload.token);
-  return axios.get(REACT_NATIVE_API + `/api/v2/medication-history/user_id/${payload.user_id}`, {
-    headers: { 'Authorization': payload.headers }
+  console.log('payload on history', payload);
+  return axios.get(REACT_NATIVE_API + `/api/v2/medication-history/user_id/${payload.id}`, {
+    headers: { 'Authorization': payload.token }
     })
     .then(response => {
-      dispatch(addMedicationHistory(response.data))
+      dispatch(getMed(response.data))
     })
     .catch(error => console.error('get all failed', error))
   }
-export const addMedicationHistory = (newMedicationHistory) => {
-  return {
-    type: 'ADD',
-    payload: newMedicationHistory
+  const getMed = payload => {
+    return {
+      type: 'GETALL',
+      payload: payload
+    }
   }
-}
-
+  
 const medicationReducer = (state = initialState, action) => {
   let { type, payload } = action;
   switch (type) {
-    case 'ADD':
+    case 'GETALL':
+      return { medication_history: payload };
 
-      return { medication_history: [...state.medication_history, payload] };
     default:
       return state;
   }
